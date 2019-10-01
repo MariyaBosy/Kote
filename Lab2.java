1e9f5d303f6b025578e565aaec06a71993d4a22f
Thomas B <32444844+tjbrett03@users.noreply.github.com>
2019-10-01 17:32:21 -0500

    Update Lab2.java

--- Lab2.java
+++ Lab2.java
@@ -1,15 +1,36 @@
 import java.util.Scanner;
 public class Lab2 {
-public static void main (String [] args) {
-	Scanner input = new Scanner(System.in);
-	System.out.println("Input String");
-	String str = input.nextLine();
-	int count = 0;
-			for(int i =0; i < str.length(); i++) {
-				count++;
-			}
-	char m = str.charAt(count/2);
-	if(count%)
-	//System.out.println(count);
-}
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input String: ");
+        String str = input.nextLine();
+        // give count value of string length
+        double count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            count++;
+        }
+        // determine if string length is odd or even
+        if (count % 2 == 0) {
+            // Even Numbers
+            count = count / 2;
+            int c = (int) Math.round(count);
+            char middle = str.charAt(c);
+            int b = c - 1;
+            char middle1 = str.charAt(b);
+            System.out.print("Middle Chars: " + middle1);
+            System.out.println(middle);
+
+        } else {
+            // Odd Numbers
+            count = count / 2;
+            count += 0.5;
+            // convert double to int
+            int c = (int) Math.round(count);
+            c--;
+            char middle = str.charAt(c);
+            System.out.println("Middle Char: " + middle);
+        }
+        //System.out.println("count:" + count);
+    }
+
 }
